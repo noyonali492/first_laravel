389b1befac4b1bcbacacb26c7058dc2e59c3b2b0
noyonali492 <noyonahmed492@gmail.com>
2024-08-10 00:14:31 +0600

    Product COtroller Add

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use App\product;
 
 class productController extends Controller
 {
     public function add(){
+
         return view('products.addProduct');
     }
 }
